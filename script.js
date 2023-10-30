a95336ad50cbfab0d686a5d3ef28edf640fa0ba2
Denisson Pereira Santos <denisson159@gmail.com>
2023-10-29 21:26:12 -0300

    up

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("expandButton").addEventListener("click", function() {
-    const overlay = document.querySelector(".overlay");
-    overlay.style.width = "100%";
-    overlay.style.height = "100%";
-    overlay.style.opacity = 0.7;
-  });
