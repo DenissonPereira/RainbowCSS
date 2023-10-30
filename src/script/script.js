e4b54c52cca2ede025d58cc9dacc5f0f670188df
Denisson Pereira Santos <denisson159@gmail.com>
2023-10-29 22:18:16 -0300

    zoom-out

--- src/script/script.js
+++ src/script/script.js
@@ -7,10 +7,12 @@ function clique() {
     elemento.style.width = '300px';
     elemento.style.height = '300px';
     elemento.style.zIndex = 1; // Volta ao valor original do z-index
+    elemento.style.cursor = 'zoom-in';
   } else {
     elemento.style.width = '3200px';
     elemento.style.height = '2500px';
     elemento.style.zIndex = 2;
+    elemento.style.cursor = 'zoom-out';
   }
 
   isExpanded = !isExpanded; // Alternar o estado
