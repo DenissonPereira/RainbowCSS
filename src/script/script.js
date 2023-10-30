96a9601da5867722f6c3ab10106bb754a777132e
Denisson Pereira Santos <denisson159@gmail.com>
2023-10-29 22:37:35 -0300

    download

--- src/script/script.js
+++ src/script/script.js
@@ -17,3 +17,21 @@ function clique() {
 
   isExpanded = !isExpanded; // Alternar o estado
 }
+
+function clique() {
+  var elemento = document.getElementsByClassName('caixa2')[0];
+
+  if (isExpanded) {
+    elemento.style.width = '300px';
+    elemento.style.height = '300px';
+    elemento.style.zIndex = 1; // Volta ao valor original do z-index
+    elemento.style.cursor = 'zoom-in';
+  } else {
+    elemento.style.width = '3200px';
+    elemento.style.height = '2500px';
+    elemento.style.zIndex = 2;
+    elemento.style.cursor = 'zoom-out';
+  }
+
+  isExpanded = !isExpanded; // Alternar o estado
+}
