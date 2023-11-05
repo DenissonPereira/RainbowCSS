8da190a372be6f60921c73c9c49785624275d2d7
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-04 22:54:50 -0300

    box32

--- src/script/script.js
+++ src/script/script.js
@@ -322,7 +322,7 @@ function coletarValor32() {
   var caixa = document.getElementById("minhaCaixa32");
 
   // Altera o ângulo do gradiente linear
-  caixa.style.backgroundImage = `linear-gradient(${valor31}deg, #12beb6, #84f064, rgb(130, 197, 43))`;
+  caixa.style.backgroundImage = `linear-gradient(${valor32}deg, #a70a0a, #ff0095);`;
 
 }
 
